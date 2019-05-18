868ba58d2b36346fb63b51fe01ea13e34f44651e
Thomas <thomas@craftacademy.se>
2019-05-18 13:28:50 +0200

    fetches employee data from back-end using axios

--- src/EmployeesList.jsx
+++ src/EmployeesList.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
-const employees = [{"id":1,"email":"george.bluth@reqres.in","first_name":"George","last_name":"Bluth","avatar":"https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"},{"id":2,"email":"janet.weaver@reqres.in","first_name":"Janet","last_name":"Weaver","avatar":"https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg"},{"id":3,"email":"emma.wong@reqres.in","first_name":"Emma","last_name":"Wong","avatar":"https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg"},{"id":4,"email":"eve.holt@reqres.in","first_name":"Eve","last_name":"Holt","avatar":"https://s3.amazonaws.com/uifaces/faces/twitter/marcoramires/128.jpg"},{"id":5,"email":"charles.morris@reqres.in","first_name":"Charles","last_name":"Morris","avatar":"https://s3.amazonaws.com/uifaces/faces/twitter/stephenmoon/128.jpg"},{"id":6,"email":"tracey.ramos@reqres.in","first_name":"Tracey","last_name":"Ramos","avatar":"https://s3.amazonaws.com/uifaces/faces/twitter/bigmancho/128.jpg"}]
 class EmployeesList extends Component {
   state = {
-    employees: employees
+    employees: []
   }
+
+  componentDidMount() {
+    this.fetchEmployees()
+  }
+
+  async fetchEmployees() {
+    let response = await axios.get('https://reqres.in/api/users?per_page=6')
+    this.setState({employees: response.data.data})
+    
+  }
+
   render() {
     console.log(this.state.employees)
     let employeesListDisplay = this.state.employees.map(employee => {
