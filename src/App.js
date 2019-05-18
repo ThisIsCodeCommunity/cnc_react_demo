5fa13fd3c6373524678f576aa6484519788d9f56
Thomas <thomas@craftacademy.se>
2019-05-18 13:54:31 +0200

    adds semantic-ui and styles the employee list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import EmployeesList from './EmployeesList'
+import { Container } from 'semantic-ui-react'
+
 
 function App() {
   return (
     <>
-      <EmployeesList />
+      <Container>
+        <EmployeesList />
+
+      </Container>
     </>
   );
 }
--- src/EmployeesList.jsx
+++ src/EmployeesList.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import { Image, List } from 'semantic-ui-react'
+
 
 class EmployeesList extends Component {
   state = {
@@ -12,21 +14,31 @@ class EmployeesList extends Component {
 
   async fetchEmployees() {
     let response = await axios.get('https://reqres.in/api/users?per_page=6')
-    this.setState({employees: response.data.data})
-    
+    this.setState({ employees: response.data.data })
+
   }
 
   render() {
     console.log(this.state.employees)
     let employeesListDisplay = this.state.employees.map(employee => {
-      return <li key={employee.id}>{employee.first_name}</li>
+      return (
+        <List.Item key={employee.id}>
+          <Image avatar src={employee.avatar} />
+          <List.Content>
+            <List.Header as='h5'>{`${employee.first_name} ${employee.last_name}`}</List.Header>
+            <List.Description>{employee.email}</List.Description>
+          </List.Content>
+
+        </List.Item>
+      )
+
     })
 
     return (
       <>
-        <ul>
+        <List relaxed>
           {employeesListDisplay}
-        </ul>
+        </List>
       </>
     );
   }
